836e2432d33dd701eff33d6c8004201aa16505cf
Amr muhammad <88667922+theclay1@users.noreply.github.com>
2024-02-24 21:23:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let form = document.querySelector(".form");
+let nameReg = /^[a-z]{2,}$/;
+let emailReg = /\w{2,}\@\w{2,}\.\w{2,}/;
+
+form.addEventListener("click", function (e) {
+  let target = e.target;
+  if (target.tagName === "INPUT") {
+    target.addEventListener("blur", function () {
+      let id = this.id;
+      if (this.value === "") {
+        this.classList.add("empty");
+      } else {
+        this.classList.remove("empty");
+      }
+      if (this.id === "email" && emailReg.test(this.value) === false) {
+        this.classList.add("empty");
+        this.placeholder = "email@example.com";
+      } else if ((id === "first-name") | (id === "last-name")) {
+        if (nameReg.test(this.value) === false) {
+          this.classList.add("empty");
+        }
+      } else if (id === "password" && this.value === "") {
+        this.classList.add("empty");
+      } else {
+        this.classList.remove("empty");
+      }
+    });
+  }
+});
